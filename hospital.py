1a7ae38b8a1c1cd789452356a61ee54711e7b20c
jsonfrench <jsonfrench@gmail.com>
2024-06-12 17:03:38 -0400

    fully functional movement

--- hospital.py
+++ hospital.py
@@ -16,7 +16,7 @@ def draw_grid():
     for i in range(SCR_HEIGHT//CELL_SIZE):
             pygame.draw.line(screen, "black", (0, i*CELL_SIZE), (SCR_WIDTH, i*CELL_SIZE),width=3)
 
-def draw_squares():
+def draw_state():
     start_x = 0
     for row in layout:
         start_y = 0
@@ -28,9 +28,21 @@ def draw_squares():
             start_y += 1
         start_x += 1
 
+def is_valid_location(position):
+     if not 0 <= position[0] < len(layout[0]) :
+          return False
+     if not 0 <= position[1] < len(layout) :
+          return False
+     if layout[position[1]][position[0]] == 1:
+          return False
+     return True
+
 CELL_SIZE = 50
 SCR_WIDTH = len(layout[0]) * CELL_SIZE
 SCR_HEIGHT = len(layout) * CELL_SIZE
+
+player_x = 0
+player_y = 0
 
 # pygame setup
 pygame.init()
@@ -41,19 +53,35 @@ running = True
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
+    
+    #Handle input
+    desired_player_x = player_x
+    desired_player_y = player_y
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                desired_player_y -= 1
+            if event.key == pygame.K_a:
+                desired_player_x -= 1
+            if event.key == pygame.K_s:
+                desired_player_y += 1
+            if event.key == pygame.K_d:
+                desired_player_x += 1
+    if is_valid_location((desired_player_x, desired_player_y)):
+         player_x = desired_player_x
+         player_y = desired_player_y
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("white")
 
     # RENDER YOUR GAME HERE
-    
-    draw_squares()
+    draw_state()
     draw_grid()
 
-    #pygame.draw.rect(screen,"black", pygame.Rect(100,100,10,10))
+    #Draw player
+    pygame.draw.circle(screen, "red", (player_x*CELL_SIZE+CELL_SIZE/2,player_y*CELL_SIZE+CELL_SIZE/2),CELL_SIZE*.9/2)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
